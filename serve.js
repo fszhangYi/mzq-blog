bcf9389309b5e25c7ed18ea3599f9f796d036b2a
Qi <92540319+fszhangYi@users.noreply.github.com>
2023-11-05 22:34:03 +0800

    add one artical

---
+++ serve.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/') {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // 允许所有来源跨域访问，根据需要可以指定具体的来源
+    res.setHeader('Access-Control-Allow-Methods', 'POST'); // 允许的请求方法
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // 允许的请求头
+
+    let data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    });
+    req.on('end', () => {
+      console.log('Received POST data:', data);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Data received successfully');
+    });
+  } else {
+    console.log('okk');
+    res.statusCode = 200;
+    res.end('get');
+  }
+});
+
+server.listen(9999, () => {
+  console.log('Server is listening on port 9999');
+});
